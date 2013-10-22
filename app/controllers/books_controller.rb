f5e9641e8bd9d6a882ef685010953aa7742b8b50
Nate Fischer <nate@compnentsearch.com>
2013-10-21 23:02:36 -0700

    refactored amazon api request into its own class

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,76 +24,19 @@ class BooksController < ApplicationController
   end
 
   def search
-		################## GOOGLE API #######################################
-		#@tempbook = Book.new
-		#@isbn = params[:search]
-
-		#if result = search_isbn(@isbn)
-		#	@tempbook.title = result['items'][0]['volumeInfo']['title']
-		#	@tempbook.author = result['items'][0]['volumeInfo']['authors'][0]
-
-		#else
-			#flash[:notice] = "Sorry ISBN#:#{@isbn} was not found" 
-			#redirect_to :back
-
-		#end
-		#####################################################################
-
-		################# AMAZON API ########################################
-		#REMEMBER TO EXTRACT PRIVATE INFO TO ENV VARIABLES 
-		req = Vacuum.new
-		  .configure(
-		    aws_access_key_id:     'AKIAIYOWFSJWSGLL7FZQ',
-		    aws_secret_access_key: '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',
-		    associate_tag:         'toen-20'
-		  )
-
-		isbn_query = params.fetch(:search, '')
-	  	strip_hyphen!(isbn_query)
+	isbn_query = params.fetch(:search, '')
+	strip_hyphen!(isbn_query)
 
 
-		unless (isbn_query.size == 10 || isbn_query.size == 13)
-			flash[:notice] = "10/13 Digit ISBN Please."
-			redirect_to root_url
-			return
-		end
-
-		@user = current_user
-		@book = Book.new
-
-		amazon_api_params = { 'Operation'   => 'ItemLookup',
-		  			 		  'ResponseGroup' => 'Large',
-           			          'SearchIndex' => 'All',
-           			          'IdType' => 'ISBN',
-           			          'ItemId' => "#{isbn_query}" }
-
-		amazon_response = req.get(query: amazon_api_params)
-		response = Response.new(amazon_response).to_h
-
-		if response['ItemLookupResponse']['Items']['Item'][0].nil?
-			@book.author           = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Author']
-			@book.publisher        = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Publisher']
-			@book.title            = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Title']
-			@book.isbn             = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['ISBN']
-			@book.amazon_url       = response['ItemLookupResponse']['Items']['Item']['DetailPageURL']
-			@book.small_image_url  = response['ItemLookupResponse']['Items']['Item']['SmallImage']['URL']
-			@book.medium_image_url = response['ItemLookupResponse']['Items']['Item']['MediumImage']['URL']
-			@book.large_image_url  = response['ItemLookupResponse']['Items']['Item']['LargeImage']['URL']
-			@book.edition          = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Edition']
-			@book.number_of_pages  = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['NumberOfPages']
-		else
-			@book.author           = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Author']
-			@book.publisher        = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Publisher']
-			@book.title            = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Title']
-			@book.isbn             = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['ISBN']
-			@book.amazon_url       = response['ItemLookupResponse']['Items']['Item'][0]['DetailPageURL']
-			@book.small_image_url  = response['ItemLookupResponse']['Items']['Item'][0]['SmallImage']['URL']
-			@book.medium_image_url = response['ItemLookupResponse']['Items']['Item'][0]['MediumImage']['URL']
-			@book.large_image_url  = response['ItemLookupResponse']['Items']['Item'][0]['LargeImage']['URL']
-			@book.edition          = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Edition']
-			@book.number_of_pages  = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['NumberOfPages']
-		end
+	unless (isbn_query.size == 10 || isbn_query.size == 13)
+		flash[:notice] = "10/13 Digit ISBN Please."
+		redirect_to root_url
+		return
 	end
+
+	@user = current_user
+	@book = AmazonSearch.new("#{isbn_query}").amazon_request
+  end
 
 	def all_books
 		@books = Book.all
---
+++ app/models/amazon_search.rb
@@ -0,0 +1,57 @@
+class AmazonSearch
+
+  def initialize(isbn)
+  	@isbn = isbn 
+  end
+
+  def amazon_request
+    req = Vacuum.new
+		  .configure(
+		    aws_access_key_id:     'AKIAIYOWFSJWSGLL7FZQ',
+		    aws_secret_access_key: '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',
+		    associate_tag:         'toen-20'
+		  )
+
+    amazon_api_params = { 'Operation'   => 'ItemLookup',
+		  			 		  'ResponseGroup' => 'Large',
+           			          'SearchIndex' => 'All',
+           			          'IdType' => 'ISBN',
+           			          'ItemId' => "#{@isbn}" }
+
+
+	amazon_response = req.get(query: amazon_api_params)
+	response = Response.new(amazon_response).to_h
+
+    book = Book.new
+
+	if response['ItemLookupResponse']['Items']['Item'][0].nil?
+		book.author           = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Author']
+		book.publisher        = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Publisher']
+		book.title            = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Title']
+		book.isbn             = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['ISBN']
+		book.amazon_url       = response['ItemLookupResponse']['Items']['Item']['DetailPageURL']
+		book.small_image_url  = response['ItemLookupResponse']['Items']['Item']['SmallImage']['URL']
+		book.medium_image_url = response['ItemLookupResponse']['Items']['Item']['MediumImage']['URL']
+		book.large_image_url  = response['ItemLookupResponse']['Items']['Item']['LargeImage']['URL']
+		book.edition          = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['Edition']
+		book.number_of_pages  = response['ItemLookupResponse']['Items']['Item']['ItemAttributes']['NumberOfPages']
+	else
+		book.author           = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Author']
+		book.publisher        = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Publisher']
+		book.title            = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Title']
+		book.isbn             = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['ISBN']
+		book.amazon_url       = response['ItemLookupResponse']['Items']['Item'][0]['DetailPageURL']
+		book.small_image_url  = response['ItemLookupResponse']['Items']['Item'][0]['SmallImage']['URL']
+		book.medium_image_url = response['ItemLookupResponse']['Items']['Item'][0]['MediumImage']['URL']
+		book.large_image_url  = response['ItemLookupResponse']['Items']['Item'][0]['LargeImage']['URL']
+		book.edition          = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['Edition']
+		book.number_of_pages  = response['ItemLookupResponse']['Items']['Item'][0]['ItemAttributes']['NumberOfPages']
+	end
+	book 
+
+  end
+
+
+
+
+end
